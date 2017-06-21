84e84a90480296a1f4ee2c24d884e3dc9656a5e0
Omar Hadary <Omar Hadary>
2017-06-21 01:49:01 -0700

    doler and file structure created, images uploaded and being used to populate the object.

---
+++ assets/javascript/app.js
@@ -0,0 +1,74 @@
+var FloorPlanImages= {
+    "Casa Nova": {
+        name: "casaNova",
+        imgURL: "assets/img/casaNova",
+        bedroomCount: 0,
+        rentAmount: 1200
+    },
+    "Monte Cristo": {
+        name: "monteCristo",
+        imgURL: "assets/img/monteCristo",
+        bedroomCount: 1,
+        rentAmount: 1650 
+    },
+    "Shangri-La": {
+        name: "shangriLa",
+        imgURL: "assets/img/shangriLa",
+        bedroomCount: 3,
+        rentAmount: 1825
+    },
+    "Versailles": {
+        name: "versailles",
+        imgURL: "assets/img/versailles",
+        bedroomCount: 2,
+        rentAmount: 4000
+    },
+    "Xanadu": {
+        name: "xanadu",
+        imgURL: "assets/img/xanadu",
+        bedroomCount: 2,
+        rentAmount: 1525
+    },
+    "Hogwarts": {
+        name: "hogwarts",
+        imgURL: "assets/img/hogwarts",
+        bedroomCount: 3,
+        rentAmount: 1825
+    },
+    "Camelot": {
+        name: "camelot",
+        imgURL: "assets/img/camelot",
+        bedroomCount: 2,
+        rentAmount: 1825
+    },
+    "El Dorado": {
+        name: "elDorado",
+        imgURL: "assets/img/elDorado",
+        bedroomCount: 1,
+        rentAmount: 1825
+    },
+    "Vanity Fair": {
+        name: "vanityFair",
+        imgURL: "assets/img/vanityFair",
+        bedroomCount: 2,
+        rentAmount: 1825
+    },
+    "Atlantis": {
+        name: "atlantis",
+        imgURL: "assets/img/atlantis",
+        bedroomCount: 1,
+        rentAmount: 1825
+    },
+    "Utopia": {
+        name: "utopia",
+        imgURL: "assets/images/utopia",
+        bedroomCount: 3,
+        rentAmount: 2825
+    },
+    "Liliput": {
+        name: "liliput",
+        imgURL: "assets/images/liliput",
+        bedroomCount: 0,
+        rentAmount: 525
+    }
+};
