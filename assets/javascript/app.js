1007866e8d4a648e2ba1fdbc42c6b5db5eee511b
Omar Hadary <Omar Hadary>
2017-06-27 01:28:12 -0700

    filters section correctly showing/hiding on mobile width. additional if statements to cover various bedroom / rent values

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -60,7 +60,8 @@ $(function() {
 		bedroomCount: 0,
 		rentAmount: 525
 	}];
-	// onload hide the reset all filters row
+	// onload hide the reset all filters and mobile screen filters rows
+	// $(".mobileFiltersButtonRow").hide();
 	$(".resetFiltersRow").hide();
 	// clear rental results 
 	$(".floorPlans").empty();
@@ -71,8 +72,8 @@ $(function() {
 		floorPlanImage.attr('src', floorPlanImagesArray[i].imgURL);
 		$(".floorPlans").append(floorPlanImage);
 	}
-	// when reset all button is clicked, reload the page
-	$(".resetAllButton").click(function() {
+	// when cancel button is clicked, reload the page
+	$(".cancelFiltersButton").click(function() {
 		location.reload();
 	});
 	// capture bedroom count input and store in localStorage
@@ -83,17 +84,20 @@ $(function() {
 		// return false;
 	});
 	// display filtered results when Apply Filters button is clicked
-	$("button.applyFiltersButton").click(function() {
+	$(".applyFiltersButton").click(function() {
 		var bedroomsToCheck = localStorage.getItem("bedroomCount");
 		var minRentToCheck = localStorage.getItem("minRentPrice");
 		var maxRentToCheck = localStorage.getItem("maxRentPrice");
-        // var minRentToCheck = parseInt(minRentToCheckRaw);
+		// var minRentToCheck = parseInt(minRentToCheckRaw);
 		// var maxRentToCheck = parseInt(maxRentToCheckRaw);
 		console.log("bedroom entered " + bedroomsToCheck);
 		console.log("min rent entered " + minRentToCheck);
 		console.log("max rent entered " + maxRentToCheck);
+		$(".resetFiltersRow").hide();
+		$("#moreFiltersButton").show();
+		$(".moreFiltersRow").hide();
 		$(".floorPlans").empty();
-        // if (minRentToCheck == null && maxRentToCheck == null) {
+		// if (minRentToCheck == null && maxRentToCheck == null) {
 		// 	console.log("rent values are null");
 		// 	for (var i = 0; i < floorPlanImagesArray.length; i++) {
 		// 		var floorPlanImage = $('<img>');
@@ -101,27 +105,25 @@ $(function() {
 		// 		$(".floorPlans").append(floorPlanImage);
 		// 	}
 		// }
-        //  {
-
-            // cvcvcv working code block for rent amount cvcvcvcv
-			// for (var j = 0; j < floorPlanImagesArray.length; j++) {
-            //     console.log("are we here");
-			// 	if (bedroomsToCheck == floorPlanImagesArray[j].bedroomCount && minRentToCheck <= floorPlanImagesArray[j].rentAmount && floorPlanImagesArray[j].rentAmount <= maxRentToCheck) {
-            //         console.log("how about here");
-			// 		console.log("rent value are not null");
-			// 		console.log("min rent after loop is " + minRentToCheck);
-			// 		console.log("max rent after loop is " + maxRentToCheck);
-            //         console.log("listing rent amount " + floorPlanImagesArray[j].rentAmount);
-			// 		var floorPlanImage = $('<img>');
-			// 		floorPlanImage.attr('src', floorPlanImagesArray[j].imgURL);
-			// 		// floorPlanImage.attr('data-type', floorPlanImagesArray[j].rentAmount);
-			// 		$(".floorPlans").append(floorPlanImage);
-			// 	}
-			// }
+		//  {
+		// cvcvcv working code block for rent amount cvcvcvcv
+		// for (var j = 0; j < floorPlanImagesArray.length; j++) {
+		//     console.log("are we here");
+		// 	if (bedroomsToCheck == floorPlanImagesArray[j].bedroomCount && minRentToCheck <= floorPlanImagesArray[j].rentAmount && floorPlanImagesArray[j].rentAmount <= maxRentToCheck) {
+		//         console.log("how about here");
+		// 		console.log("rent value are not null");
+		// 		console.log("min rent after loop is " + minRentToCheck);
+		// 		console.log("max rent after loop is " + maxRentToCheck);
+		//         console.log("listing rent amount " + floorPlanImagesArray[j].rentAmount);
+		// 		var floorPlanImage = $('<img>');
+		// 		floorPlanImage.attr('src', floorPlanImagesArray[j].imgURL);
+		// 		// floorPlanImage.attr('data-type', floorPlanImagesArray[j].rentAmount);
+		// 		$(".floorPlans").append(floorPlanImage);
+		// 	}
 		// }
-
-// XZXZX good code block ZXZXZX
-
+		// }
+		// XZXZX good code block ZXZXZX
+		// if statement to filter if no bedroom or rent amounts are selected
 		if (bedroomsToCheck == null && minRentToCheck == null && maxRentToCheck == null) {
 			console.log("rent values are null");
 			for (var i = 0; i < floorPlanImagesArray.length; i++) {
@@ -130,6 +132,7 @@ $(function() {
 				$(".floorPlans").append(floorPlanImage);
 			}
 		}
+		// if statement to filter by bedrooms >= 3
 		if (bedroomsToCheck == 3 && minRentToCheck !== null && maxRentToCheck !== null) {
 			console.log("rent value are not null");
 			console.log("min rent is " + minRentToCheck);
@@ -143,6 +146,62 @@ $(function() {
 					console.log(floorPlanImagesArray[i].rentAmount);
 				}
 			}
+		}
+		// if statement to filter if bedroom count is not selected
+		if (bedroomsToCheck == null && minRentToCheck !== null && maxRentToCheck !== null) {
+			for (var i = 0; i < floorPlanImagesArray.length; i++) {
+				if (minRentToCheck <= floorPlanImagesArray[i].rentAmount && floorPlanImagesArray[i].rentAmount <= maxRentToCheck) {
+					console.log("rent value are not null");
+					console.log("min rent is " + minRentToCheck);
+					console.log("max rent is " + maxRentToCheck);
+					var floorPlanImage = $('<img>');
+					floorPlanImage.attr('src', floorPlanImagesArray[i].imgURL);
+					$(".floorPlans").append(floorPlanImage);
+					console.log(floorPlanImage);
+				}
+			}
+		}
+		// if statement to filter if rent amount is not selected
+		if (bedroomsToCheck !== null && minRentToCheck == null && maxRentToCheck == null) {
+			for (var i = 0; i < floorPlanImagesArray.length; i++) {
+				if (bedroomsToCheck == floorPlanImagesArray[i].bedroomCount) {
+					console.log("rent value are not null");
+					console.log("min rent is " + minRentToCheck);
+					console.log("max rent is " + maxRentToCheck);
+					var floorPlanImage = $('<img>');
+					floorPlanImage.attr('src', floorPlanImagesArray[i].imgURL);
+					$(".floorPlans").append(floorPlanImage);
+					console.log(floorPlanImage);
+				}
+			}
+		}
+		// // if statement to filter if min rent amount is not selected
+		if (bedroomsToCheck !== null && minRentToCheck == null && maxRentToCheck !== null) {
+			for (var i = 0; i < floorPlanImagesArray.length; i++) {
+				if (bedroomsToCheck == floorPlanImagesArray[i].bedroomCount && floorPlanImagesArray[i].rentAmount <= maxRentToCheck) {
+					console.log("rent value are not null");
+					console.log("min rent is " + minRentToCheck);
+					console.log("max rent is " + maxRentToCheck);
+					var floorPlanImage = $('<img>');
+					floorPlanImage.attr('src', floorPlanImagesArray[i].imgURL);
+					$(".floorPlans").append(floorPlanImage);
+					console.log(floorPlanImage);
+				}
+			}
+		}
+		// if statement to filter if max rent amount is not selected
+		if (bedroomsToCheck !== null && minRentToCheck !== null && maxRentToCheck == null) {
+			for (var i = 0; i < floorPlanImagesArray.length; i++) {
+				if (bedroomsToCheck == floorPlanImagesArray[i].bedroomCount && minRentToCheck <= floorPlanImagesArray[i].rentAmount) {
+					console.log("rent value are not null");
+					console.log("min rent is " + minRentToCheck);
+					console.log("max rent is " + maxRentToCheck);
+					var floorPlanImage = $('<img>');
+					floorPlanImage.attr('src', floorPlanImagesArray[i].imgURL);
+					$(".floorPlans").append(floorPlanImage);
+					console.log(floorPlanImage);
+				}
+			}
 		} else {
 			for (var i = 0; i < floorPlanImagesArray.length; i++) {
 				if (bedroomsToCheck == floorPlanImagesArray[i].bedroomCount && minRentToCheck <= floorPlanImagesArray[i].rentAmount && floorPlanImagesArray[i].rentAmount <= maxRentToCheck) {
@@ -150,8 +209,6 @@ $(function() {
 					console.log("min rent is " + minRentToCheck);
 					console.log("max rent is " + maxRentToCheck);
 					var floorPlanImage = $('<img>');
-					floorPlanImage.attr('src', floorPlanImagesArray[i].imgURL);
-					floorPlanImage.attr('data-type', floorPlanImagesArray[i].rentAmount);
 					floorPlanImage.attr('src', floorPlanImagesArray[i].imgURL);
 					$(".floorPlans").append(floorPlanImage);
 					console.log(floorPlanImage);
@@ -205,11 +262,10 @@ $(function() {
 			slide: function(event, ui) {
 				$("#minRentPrice").val("$" + ui.values[0]);
 				$("#maxRentPrice").val(" - $" + ui.values[1]);
+				localStorage.setItem("minRentPrice", ui.values[0]);
+				localStorage.setItem("maxRentPrice", ui.values[1]);
 			},
-			stop: function(event, ui) {
-					localStorage.setItem("minRentPrice", ui.values[0]);
-					localStorage.setItem("maxRentPrice", ui.values[1]);
-				}
+			stop: function(event, ui) {}
 				// ,
 				// create: function(event, ui) {
 				//         var minValue=$(this).slider('values',0);
@@ -260,8 +316,8 @@ $(function() {
 		$("#minArea").val($("#sliderSQFT").slider("values", 0) + " sq.ft. ");
 		$("#maxArea").val("- " + $("#sliderSQFT").slider("values", 1) + " sq.ft.");
 	});
-	// clear all selected filters
-	$("button.cancelFiltersButton").click(function() {
+	// clear all selected filters when reset all button is clicked
+	$(".resetAllButton").click(function() {
 		localStorage.clear();
 		$("#sliderRentAmount").slider("option", "values", [500, 4000]);
 		$("#minRentPrice").val("$" + $("#sliderRentAmount").slider("values", 0));
@@ -273,18 +329,48 @@ $(function() {
 	// show row with reset all filters button and hide more filters button
 	$("#moreFiltersButton").click(function() {
 		$(".resetFiltersRow").show();
+		$(".moreFiltersRow").show();
 		$("#moreFiltersButton").hide();
 	});
 	// add more amenities
 	$("#moreAmenitiesButton").click(function() {
-			$("#leftAmenitiesList").append("<div class='checkbox'><label><input type='checkbox' value=''>SWIMMING POOL</label></div><div class='checkbox'><label><input type='checkbox' value=''>WIRELESS INTERNET</label></div><div class='checkbox'><label><input type='checkbox' value=''>HOT COFFEE</label></div><div class='checkbox'><label><input type='checkbox' value=''>SECURITY SYSTEM</label></div>");
-			$("#rightAmenitiesList").append("<div class='checkbox'><label><input type='checkbox' value=''>FIREPLACE</label></div><div class='checkbox'><label><input type='checkbox' value=''>CABLE TELEVISION</label></div><div class='checkbox'><label><input type='checkbox' value=''>CLIMATE CONTROLLED</label></div><div class='checkbox'><label><input type='checkbox' value=''>TENNIS COURTS</label></div>");
-			$("#moreAmenitiesButton").replaceWith("<div class='col-md-6 col-md-offset-6' role='button'>Less</div>");
-		})
-		// function to use bedroom count onclick
-		//      on release of bedroom count button, clear the #floor-plans, loop through the floorPlansImages and only push the results matching
-		// function to use rent toggle
-		// on release, check value of the newly position bracket, clear the results in the and loop through the floorPlans images to update the results in the #floor-plan
-		// On Click More Filters, expand the filters area. On click of cancel button, go back to minimal filters area, onclick of apply button, filter the floorPlanImages by the bedroom and rent parameters already entered
-		// at mobile screen, only show a filters button and expand the filters screen once clicked
+		$("#leftAmenitiesList").append("<div class='checkbox'><label><input type='checkbox' value=''>SWIMMING POOL</label></div><div class='checkbox'><label><input type='checkbox' value=''>WIRELESS INTERNET</label></div><div class='checkbox'><label><input type='checkbox' value=''>HOT COFFEE</label></div><div class='checkbox'><label><input type='checkbox' value=''>SECURITY SYSTEM</label></div>");
+		$("#rightAmenitiesList").append("<div class='checkbox'><label><input type='checkbox' value=''>FIREPLACE</label></div><div class='checkbox'><label><input type='checkbox' value=''>CABLE TELEVISION</label></div><div class='checkbox'><label><input type='checkbox' value=''>CLIMATE CONTROLLED</label></div><div class='checkbox'><label><input type='checkbox' value=''>TENNIS COURTS</label></div>");
+		$("#moreAmenitiesButton").replaceWith("<div class='col-md-6 col-md-offset-6' role='button'>Less</div>");
+	});
+	// sections to show/hide at mobile width
+	if ($(window).width() <= 425) {
+		// alert("less than 768");
+		$(".mobileFiltersButtonRow").show();
+		$(".landingPageFiltersRow").hide();
+        // hide large screen filters section when filters are applied
+        $(".applyFiltersButton").click(function() {
+		$(".filtersSection").css("height", "18px");
+        console.log("SDSDFGSFG clicked");
+        $(".landingPageFiltersRow").hide();
+        $(".mobileFiltersButtonRow").show();
+	});
+    // $(".mobileFiltersButtonRow").show();
+		// $(".largeScreenFiltersSection").hide();
+		// $("#accordion").hide();
+	}
+	if ($(window).width() > 425) {
+		// alert("more than 768");
+		$(".mobileFiltersButtonRow").hide();
+		// $(".largeScreenFiltersSection").show();
+	}
+	// make filters section full screen on mobile width when mobile filters button is clicked
+	$("#mobileFiltersButton").click(function() {
+        // $("#mobileFiltersButton").hide();
+        $(".landingPageFiltersRow").show();
+        $(".moreFiltersRow").show();
+        $("#moreFiltersButton").hide();
+		$(".filtersSection").css("height", $(window).height());
+	});
+	// function to use bedroom count onclick
+	//      on release of bedroom count button, clear the #floor-plans, loop through the floorPlansImages and only push the results matching
+	// function to use rent toggle
+	// on release, check value of the newly position bracket, clear the results in the and loop through the floorPlans images to update the results in the #floor-plan
+	// On Click More Filters, expand the filters area. On click of cancel button, go back to minimal filters area, onclick of apply button, filter the floorPlanImages by the bedroom and rent parameters already entered
+	// at mobile screen, only show a filters button and expand the filters screen once clicked
 });
